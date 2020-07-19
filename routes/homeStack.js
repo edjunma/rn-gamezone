26a5a244a0125f81fc87a11ca6d4f8ccdb694b5d
Edmond Ma <edjunma@gmail.com>
2020-07-18 23:02:22 -0400

    add react-drawer navigation

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,18 +1,28 @@
 import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation';
 import Home from '../screens/home';
 import ReviewDetails from '../screens/reviewDetails';
 
 const screens = {
 	Home: {
 		screen: Home,
+		navigationOptions: {
+			title: 'GameZone',
+		},
 	},
 	ReviewDetails: {
 		screen: ReviewDetails,
+		navigationOptions: {
+			title: 'Review Details',
+		},
 	},
 };
 
 // home stack navigator screens
-const HomeStack = createStackNavigator(screens);
+const HomeStack = createStackNavigator(screens, {
+	defaultNavigationOptions: {
+		headerTintColor: '#444',
+		headerStyle: { backgroundColor: '#eee', height: 60 },
+	},
+});
 
-export default createAppContainer(HomeStack);
+export default HomeStack;
