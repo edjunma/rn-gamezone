b9a771d7e0810aeff0f71ea9f93864cfb622fef6
Edmond Ma <edjunma@gmail.com>
2020-07-18 22:46:34 -0400

    fixed maskedview error, still working on stack navigator

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
 import React, { useState } from 'react';
-import Home from './screens/home';
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
+import Navigator from './routes/homeStack';
 
-const getFonts = () => {
-	return Font.loadAsync({
+const getFonts = () =>
+	Font.loadAsync({
 		'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
 		'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
 	});
-};
 
 export default function App() {
 	const [fontsLoaded, setFontsLoaded] = useState(false);
 
 	if (fontsLoaded) {
-		return <Home />;
+		return <Navigator />;
 	} else {
 		return <AppLoading startAsync={getFonts} onFinish={() => setFontsLoaded(true)} />;
 	}
