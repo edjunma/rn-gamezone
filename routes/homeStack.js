4493cb7d042ea0e3d3cde3cd0ba7c3c1d2c70633
Edmond Ma <edjunma@gmail.com>
2020-07-19 00:36:35 -0400

    add in hamburger drawer navigation

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,12 +1,16 @@
 import { createStackNavigator } from 'react-navigation-stack';
+import React from 'react';
+import Header from '../shared/header';
 import Home from '../screens/home';
 import ReviewDetails from '../screens/reviewDetails';
 
 const screens = {
 	Home: {
 		screen: Home,
-		navigationOptions: {
-			title: 'GameZone',
+		navigationOptions: ({ navigation }) => {
+			return {
+				headerTitle: () => <Header title='GameZone' navigation={navigation} />,
+			};
 		},
 	},
 	ReviewDetails: {
