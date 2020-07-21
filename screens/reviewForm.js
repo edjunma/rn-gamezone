32a2803fa74e1ccb8fa7129e7a68eb8c9171d8e7
Edmond Ma <edjunma@gmail.com>
2020-07-20 21:59:11 -0400

    finish tutorial project

--- screens/reviewForm.js
+++ screens/reviewForm.js
@@ -3,6 +3,7 @@ import { StyleSheet, Button, TextInput, View, Text } from 'react-native';
 import { globalStyles } from '../styles/global.js';
 import { Formik } from 'formik';
 import * as yup from 'yup';
+import FlatButton from '../shared/button.js';
 
 const reviewSchema = yup.object({
 	title: yup.string().required().min(4),
@@ -32,18 +33,20 @@ export default function ReviewForm({ addReview }) {
 							style={globalStyles.input}
 							placeholder='Review title'
 							onChangeText={props.handleChange('title')}
-							value={props.values.title}
 							onBlur={props.handleBlur('title')}
+							value={props.values.title}
 						/>
+						{/* only if the left value is a valid string, will the right value be displayed */}
 						<Text style={globalStyles.errorText}>{props.touched.title && props.errors.title}</Text>
 
 						<TextInput
 							style={globalStyles.input}
 							multiline
+							minHeight={60}
 							placeholder='Review details'
 							onChangeText={props.handleChange('body')}
-							value={props.values.body}
 							onBlur={props.handleBlur('body')}
+							value={props.values.body}
 						/>
 						<Text style={globalStyles.errorText}>{props.touched.body && props.errors.body}</Text>
 
@@ -51,15 +54,15 @@ export default function ReviewForm({ addReview }) {
 							style={globalStyles.input}
 							placeholder='Rating (1 - 5)'
 							onChangeText={props.handleChange('rating')}
+							onBlur={props.handleBlur('rating')}
 							value={props.values.rating}
 							keyboardType='numeric'
-							onBlur={props.handleBlur('rating')}
 						/>
 						<Text style={globalStyles.errorText}>
 							{props.touched.rating && props.errors.rating}
 						</Text>
 
-						<Button color='maroon' title='Submit' onPress={props.handleSubmit} />
+						<FlatButton onPress={props.handleSubmit} text='submit' />
 					</View>
 				)}
 			</Formik>
