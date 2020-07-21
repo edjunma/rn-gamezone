6390b8d161e8e0d8da6e9ae4102b5b53705695ae
Edmond Ma <edjunma@gmail.com>
2020-07-20 21:43:32 -0400

    finish form validation, work on custom button

--- screens/home.js
+++ screens/home.js
@@ -1,5 +1,14 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, FlatList, Modal } from 'react-native';
+import {
+	StyleSheet,
+	View,
+	Text,
+	TouchableOpacity,
+	FlatList,
+	Modal,
+	TouchableWithoutFeedback,
+	Keyboard,
+} from 'react-native';
 import { globalStyles } from '../styles/global';
 import { MaterialIcons } from '@expo/vector-icons';
 import Card from '../shared/card';
@@ -25,15 +34,17 @@ export default function Home({ navigation }) {
 	return (
 		<View style={globalStyles.container}>
 			<Modal visible={modalOpen} animationType='slide'>
-				<View style={styles.modalContent}>
-					<MaterialIcons
-						name='close'
-						size={24}
-						style={{ ...styles.modalToggle, ...styles.modalClose }}
-						onPress={() => setModalOpen(false)}
-					/>
-					<ReviewForm addReview={addReview} />
-				</View>
+				<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+					<View style={styles.modalContent}>
+						<MaterialIcons
+							name='close'
+							size={24}
+							style={{ ...styles.modalToggle, ...styles.modalClose }}
+							onPress={() => setModalOpen(false)}
+						/>
+						<ReviewForm addReview={addReview} />
+					</View>
+				</TouchableWithoutFeedback>
 			</Modal>
 
 			<MaterialIcons
--- screens/reviewForm.js
+++ screens/reviewForm.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import { StyleSheet, Button, TextInput, View, Text } from 'react-native';
-import { globalStyles } from '../styles/global';
+import { globalStyles } from '../styles/global.js';
 import { Formik } from 'formik';
+import * as yup from 'yup';
+
+const reviewSchema = yup.object({
+	title: yup.string().required().min(4),
+	body: yup.string().required().min(8),
+	rating: yup
+		.string()
+		.required()
+		.test('is-num-1-5', 'Rating must be a number 1 - 5', (val) => {
+			return parseInt(val) < 6 && parseInt(val) > 0;
+		}),
+});
 
 export default function ReviewForm({ addReview }) {
 	return (
 		<View style={globalStyles.container}>
 			<Formik
-				initialValues={{ title: '', body: '', rating: ' ' }}
-				onSubmit={(values) => {
+				initialValues={{ title: '', body: '', rating: '' }}
+				validationSchema={reviewSchema}
+				onSubmit={(values, actions) => {
+					actions.resetForm();
 					addReview(values);
 				}}
 			>
@@ -18,27 +32,34 @@ export default function ReviewForm({ addReview }) {
 							style={globalStyles.input}
 							placeholder='Review title'
 							onChangeText={props.handleChange('title')}
-							value={props.value.title}
+							value={props.values.title}
+							onBlur={props.handleBlur('title')}
 						/>
+						<Text style={globalStyles.errorText}>{props.touched.title && props.errors.title}</Text>
 
 						<TextInput
-							multiline
 							style={globalStyles.input}
-							placeholder='Review body'
+							multiline
+							placeholder='Review details'
 							onChangeText={props.handleChange('body')}
-							value={props.value.body}
+							value={props.values.body}
+							onBlur={props.handleBlur('body')}
 						/>
+						<Text style={globalStyles.errorText}>{props.touched.body && props.errors.body}</Text>
 
 						<TextInput
-							multiline
 							style={globalStyles.input}
-							placeholder='Rating (1-5)'
+							placeholder='Rating (1 - 5)'
 							onChangeText={props.handleChange('rating')}
-							value={props.value.rating}
+							value={props.values.rating}
 							keyboardType='numeric'
+							onBlur={props.handleBlur('rating')}
 						/>
+						<Text style={globalStyles.errorText}>
+							{props.touched.rating && props.errors.rating}
+						</Text>
 
-						<Button title='submit' color='maroon' onPress={props.handleSubmit} />
+						<Button color='maroon' title='Submit' onPress={props.handleSubmit} />
 					</View>
 				)}
 			</Formik>
